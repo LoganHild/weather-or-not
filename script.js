49e852f52f3524573506be006876049311276757
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-07-23 12:29:30 -0500

    added some more comments to help with layout, did a bit of css on the header

---
+++ script.js
@@ -0,0 +1,5 @@
+//use what we learned in wednesdays example to add the results from the search bar to the API where it says city name.
+//Widget for the search bar? 
+//add searches to localStorage for easy searching.
+//added underneath searchbar in <aside> to be clicked. Maybe do it how the last assignment was done with assigning it a data-value and using that to getItem
+//when stored searches are clicked, same process happens as if it was searched. 
